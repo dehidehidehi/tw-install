903f2807161a86e2c11445425dded78d99fe3f22
dehi <40899632+dehidehidehi@users.noreply.github.com>
2023-10-15 01:46:19 +0200

    Update helper.sh

--- src/helper.sh
+++ src/helper.sh
@@ -34,3 +34,21 @@ read_user_key_from_log() {
   local result=${value:14:36}
   echo "$result"
 }
+
+swap_line_containing_string() {
+  local old_line_pattern=$1; shift
+  local new_line=$1; shift
+  local file=$1
+  local new;
+  new=$(echo "${new_line}" | sed 's/\//\\\//g')
+  touch "${file}"
+  sed -i '/'"${old_line_pattern}"'/{s/.*/'"${new}"'/;h};${x;/./{x;q100};x}' "${file}"
+  
+  # Check if the exit status of the previous command is not equal to 100
+  # and if the variable ${new_line} is not an empty string
+  if [[ $? -ne 100 ]] && [[ ${new_line} != '' ]]; then
+    # If both conditions are met, append the value of ${new_line} to the
+    # file specified by ${file}
+    echo "${new_line}" >> "${file}"
+  fi
+}
